e64361a03957bb91bc7039ff581dede676bbb96f
Guilherme-j10 <guilhermeZA.123123@gmail.com>
2019-12-03 00:32:54 -0300

    finalizado

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Stones of space</title>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <link rel="stylesheet" href="style/style.css">
+    </head>
+    <body>  
+        <main>
+            <header>
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a id="ativado" href="#">SYNOPSIS</a></li>
+                    <li><a href="#">CAST</a></li>
+                    <li><a href="#">TRAILER</a></li>
+                </ul>
+            </header>
+            <nav>
+                <div class="logo">
+                    <a href="#"><img src="img/avengers.png" alt="logo"></a>
+                </div>
+                <ul>
+                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fas fa-envelope"></i></a></li>
+                </ul>
+                <div class="end">
+                    <a href="#"><i class="fas fa-angle-double-down"></i></a>
+                </div>
+            </nav>
+            <aside>
+                <div class="menu">
+                    <a href="#"><i class="fas fa-bars"></i></a>
+                </div>
+                <div class="end">
+                    <a href="#">ESP</a>
+                </div>
+            </aside>
+            <article>
+                <div class="sinopse">
+                    <div class="box">
+                        <h1><strong>Space gem</strong> GEM</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dicta at, architecto impedit labore corrupti laudantium distinctio esse obcaecati, re harum?</p>
+                        <a href="#">ver mais</a>
+                    </div>
+                </div>
+                <header>
+                    <h1>SPACE</h1>
+                    <h3>Stone</h3>
+                </header>
+                <div class="historia">
+                    <div class="box">
+                        <h1><strong>stone</strong> history</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dicta at, architecto impedit labore corrupti laudantium distinctio esse obcaecati, re harum?</p>
+                        <a href="#">ver mais</a>
+                    </div>
+                </div>
+            </article>
+            <footer>
+                <div class="arrow">
+                    <a href="#"><i class="fas fa-chevron-left"></i></a>
+                </div>
+                <ul>
+                    <li><a href="#">soul gem</a></li>
+                    <li><a href="#">reality gem</a></li>
+                    <li><a id="ativo" href="#">space gem</a></li>
+                    <li><a href="#">power gem</a></li>
+                    <li><a href="#">time gem</a></li>
+                    <li><a href="#">mind gem</a></li>
+                </ul>
+                <div class="arrow2">
+                    <a href="#"><i class="fas fa-chevron-right"></i></a>
+                </div>
+            </footer>
+        </main>
+    </body>
+</html>
